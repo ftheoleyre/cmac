8c0aca98f03297db91acc9cfb9ce821aab4bd2f9
Fabrice Theoleyre <theoleyre@unistra.fr>
2009-11-13 15:57:27 +0000

    scripts files + less debug in printf
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cmac/trunk@98 82507c83-fcc2-4493-92f8-b30dc6791bf2

--- scripts/sim_exe/topo_sim.sh
+++ scripts/sim_exe/topo_sim.sh
@@ -42,14 +42,14 @@ SINK_ADDR=`cat $FILE_PARAMS | grep "^[^#]" | grep sink_address | cut -d '=' -f 2
 echo "Sink Address" $SINK_ADDR
 
 #Cmac-parameters
-KTREE_ALGO_LIST=`cat $FILE_PARAMS | grep "^[^#]" | grep Ktree_Algo | cut -d '=' -f 2`
+KTREE_ALGO_LIST=`cat $FILE_PARAMS | grep "^[^#]" | grep Ktree_algo | cut -d '=' -f 2`
 echo "KTREE ALGO IDS" $KTREE_ALGO_LIST
 NB_CHANNELS_LIST=`cat $FILE_PARAMS | grep "^[^#]" | grep Nb_channels | cut -d '=' -f 2`
-echo "NB CHANNELS" $NB_CHANNELS_LIST
+echo "NB_CHANNELS" $NB_CHANNELS_LIST
 NB_BRANCHES_LIST=`cat $FILE_PARAMS | grep "^[^#]" | grep Nb_branches | cut -d '=' -f 2`
-echo "NB BRANCHES" $NB_BRANCHES_LIST
+echo "NB_BRANCHES" $NB_BRANCHES_LIST
 MAX_TIME_PRIV_MODE_LIST=`cat $FILE_PARAMS | grep "^[^#]" | grep Max_time_priv_mode | cut -d '=' -f 2`
-echo "NB BRANCHES" $MAX_TIME_PRIV_MODE_LIST
+echo "MAX_TIME_PRIV_MODE" $MAX_TIME_PRIV_MODE_LIST
 
 #Positions
 X_MAX_LIST=`cat $FILE_PARAMS | grep "^[^#]" | grep X_MAX | cut -d '=' -f 2`
@@ -57,12 +57,11 @@ echo "XMAX" $X_MAX_LIST
 POS_METHOD=`cat $FILE_PARAMS | grep "^[^#]" | grep Positions_Method | cut -d '=' -f 2`
 
 #name for the file
-TMP=`cat $FILE_PARAMS | grep "^[^#]" | grep Positions_Method | cut -d '=' -f 2`
-SIMULATION_NAME=$TOPO"_"$TMP
+SIMULATION_NAME=$TOPO
 echo "SIMULATION NAME" $SIMULATION_NAME
 
 #ARGS
-ARGS="-net_name $TOPO -ef cmac_generic.ef -noprompt"
+ARGS="-net_name cmac-$TOPO -ef cmac_generic.ef -noprompt"
 ARGS="$ARGS -duration $DURATION"
 ARGS="$ARGS -Positions_Method $POS_METHOD"
 
@@ -84,45 +83,50 @@ do
 			do
 				for X_MAX in $X_MAX_LIST;
 				do
-				for NB_CHANNELS in $NB_CHANNELS_LIST;
-				do
-				for NB_BRANCHES in $NB_BRANCHES_LIST;
-				do
-				for MAX_TIME_PRIV_MODE in $MAX_TIME_PRIV_MODE_LIST;
-				do
-							#one random seed
-							SEED=`hexdump -n4 -e\"%u\" /dev/random`
-							RES_TMP_DIR="/tmp/debug/$TOPO/$SEED"
-							mkdir -p $RES_TMP_DIR
+					for NB_CHANNELS in $NB_CHANNELS_LIST;
+					do
+						for NB_BRANCHES in $NB_BRANCHES_LIST;
+						do
+							for MAX_TIME_PRIV_MODE in $MAX_TIME_PRIV_MODE_LIST;
+							do
+							
+								#one random seed
+								SEED=`hexdump -n4 -e\"%u\" /dev/random`
+								RES_TMP_DIR="/tmp/debug/$TOPO/$SEED"
+								mkdir -p $RES_TMP_DIR
 
-							#simulation arguments for this run
-							CMD="op_runsim -Mac_Layer $MAC_LAYER -seed $SEED -Result_Directory $RES_TMP_DIR"
-							CMD="$CMD -interpacket_time $INTER_PK_TIME"
-							CMD="$CMD -ktree_algo $KTREE_ALGO"
-							CMD="$CMD -max_time_priv_mode $MAX_TIME_PRIV_MODE"
-							CMD="$CMD -nb_branches $NB_BRANCHES -nb_channels $NB_CHANNELS"
-							CMD="$CMD -X_MAX $X_MAX"
-							CMD="$CMD -sink_address $SINK_ADDR"
-							CMD="$CMD $ARGS"				
-							echo $CMD " > " $RES_TMP_DIR
-							$CMD
+								#simulation arguments for this run
+								CMD="op_runsim -Mac_Layer $MAC_LAYER -seed $SEED -Result_Directory $RES_TMP_DIR"
+								CMD="$CMD -interpacket_time $INTER_PK_TIME"
+								CMD="$CMD -ktree_algo $KTREE_ALGO"
+								CMD="$CMD -max_time_priv_mode $MAX_TIME_PRIV_MODE"
+								CMD="$CMD -nb_branches $NB_BRANCHES -nb_channels $NB_CHANNELS"
+								CMD="$CMD -X_MAX $X_MAX"
+								CMD="$CMD -sink_address $SINK_ADDR"
+								CMD="$CMD $ARGS"				
+								echo $CMD " > " $RES_TMP_DIR
+								$CMD
 			
-							#I must move the results in the correct place
-							RES_DIR="../results/$SIMULATION_NAME/${MAC_LAYER_NAME[${MAC_LAYER}]}/"
-							echo $RES_DIR
-							if [ ! -d $RES_DIR ]
-							then
-								mkdir -p $RES_DIR
-							fi
+								#I must move the results in the correct place
+								RES_DIR="../results/$SIMULATION_NAME/${MAC_LAYER_NAME[${MAC_LAYER}]}/"
+								echo $RES_DIR
+								if [ ! -d $RES_DIR ]
+								then
+									mkdir -p $RES_DIR
+								fi
 						
-							#copy the resulting files
-							mv $RES_TMP_DIR $RES_DIR
+								#copy the resulting files
+								mv $RES_TMP_DIR $RES_DIR
 						
-							#extract the correct stats
-							MAC_FILE="`ls $RES_DIR/$SEED/*stats-nodes*`"
-							CMD="./extract_values_from_result_file.sh $MAC_FILE $SEED $RES_DIR"
-							echo $CMD; $CMD
-							#rm -rf $RES_TMP_DIR
+								#extract the correct stats
+								MAC_FILE="`ls $RES_DIR/$SEED/*stats-nodes*`"
+								CMD="./extract_values_from_result_file.sh $MAC_FILE $SEED $RES_DIR"
+								echo $CMD
+								#$CMD
+								#rm -rf $RES_TMP_DIR
+							done
+						done
+					done
 				done
 			done
 		done
