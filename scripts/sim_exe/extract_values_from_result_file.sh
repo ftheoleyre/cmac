01994a0d9530bf780847f0a95ed42aaa1ac7c8ec
Fabrice Theoleyre <theoleyre@unistra.fr>
2010-02-26 16:16:51 +0000

    new simulations
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cmac/trunk@136 82507c83-fcc2-4493-92f8-b30dc6791bf2

--- scripts/sim_exe/extract_values_from_result_file.sh
+++ scripts/sim_exe/extract_values_from_result_file.sh
@@ -23,6 +23,7 @@ NB_NODES=`cat $MAC_FILE |  grep "Number of nodes"  |cut -d ":" -f 2`
 SIM_LENGTH=`cat $MAC_FILE |  grep "X_MAX"  |cut -d ":" -f 2`
 INTER_PK_T=`cat $MAC_FILE |  grep "Inter Packet Time"  |cut -d ":" -f 2`
 DEGREE=`cat $MAC_FILE |  grep "Average degree"  |cut -d ":" -f 2`
+BEB=`cat $MAC_FILE |  grep "Exponential backoff"  |cut -d ":" -f 2`
 
 #MAC parameters
 NB_CHANNELS=`cat $MAC_FILE |  grep "Number of channels"  |cut -d ":" -f 2`
@@ -33,8 +34,10 @@ CTR_HOPS=`cat $MAC_FILE |  grep "CTR hop spacing"  |cut -d ":" -f 2`
 DRATIO_DATA=`cat $MAC_FILE |  grep "Delivery Ratio (data)"  |cut -d ":" -f 2`
 DELAY_AVG=`cat $MAC_FILE |  grep "Average Delay (in s)"  |cut -d ":" -f 2`
 DELAY_STDDEV=`cat $MAC_FILE |  grep "Delay Standard Deviation (in s)"  |cut -d ":" -f 2`
+DELAY_MAX=`cat $MAC_FILE |  grep "Max Delay (in s)"  |cut -d ":" -f 2`
 JAIN_DRATIO=`cat $MAC_FILE |  grep "Delivery Ratio Jain Index"  |cut -d ":" -f 2`
 GOODPUT_MBPS=`cat $MAC_FILE |  grep "Goodput (in Mbps)"  |cut -d ":" -f 2`
+RLENGTH=`cat $MAC_FILE |  grep "Average route length"  |cut -d ":" -f 2`
 
 #Error detection
 NB_PKTS=`cat $MAC_FILE |  grep "Nb packets sent"  |cut -d ":" -f 2`
@@ -43,11 +46,11 @@ NB_PKTS=`cat $MAC_FILE |  grep "Nb packets sent"  |cut -d ":" -f 2`
 if [ $NB_PKTS \> 0 ]
 then
 	#file to save results
-	FILE_RESULT_AGGREG="$3/nodes=`echo $NB_NODES`_length=`echo $SIM_LENGTH`_algo=`echo $MAC_LAYER`-`echo $KTREE_ALGO`_nbchannels=`echo $NB_CHANNELS`.txt"
+	FILE_RESULT_AGGREG="$3/nodes=`echo $NB_NODES`_length=`echo $SIM_LENGTH`_algo=`echo $MAC_LAYER`-`echo $KTREE_ALGO`_nbchannels=`echo $NB_CHANNELS`_beb=`echo $BEB`.txt"
 	echo $FILE_RESULT_AGGREG
 	
 	#all the stats
-	echo "$NB_NODES	$SIM_LENGTH	$INTER_PK_T	$DEGREE	$NB_CHANNELS	$NB_BRANCHES	$CTR_HOPS	$DRATIO_DATA	$DELAY_AVG	$DELAY_STDDEV	$JAIN_DRATIO	$GOODPUT_MBPS	$SEED" >> $FILE_RESULT_AGGREG
+	echo "$NB_NODES	$SIM_LENGTH	$INTER_PK_T	$DEGREE	$NB_CHANNELS	$NB_BRANCHES	$CTR_HOPS	$DRATIO_DATA	$DELAY_AVG	$DELAY_STDDEV	$DELAY_MAX	$JAIN_DRATIO	$GOODPUT_MBPS	$RLENGTH	$SEED" >> $FILE_RESULT_AGGREG
 fi
 
 
