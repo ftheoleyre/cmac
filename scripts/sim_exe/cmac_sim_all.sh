c72fdae9ce37fb0f60fdbe584f4c4667dd071bbf
Fabrice Theoleyre <theoleyre@unistra.fr>
2010-03-03 10:42:15 +0000

    new script for random topologies
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/cmac/trunk@138 82507c83-fcc2-4493-92f8-b30dc6791bf2

--- scripts/sim_exe/cmac_sim_all.sh
+++ scripts/sim_exe/cmac_sim_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+#grid 7x7
 nohup nice -n 20  ./cmac_sim.sh grid7x7_load &
+
+#tuning parameter's values
 nohup nice -n 20 ./cmac_sim.sh grid7x7_ctrhop &
 nohup nice -n 20 ./cmac_sim.sh grid7x7_branches &
 nohup nice -n 20 ./cmac_sim.sh grid7x7_privtime &
+
+#proof of correctness for the k-tree algo
 nohup nice -n 20 ./cmac_sim.sh grid7x7_static-dynamic &
 
+#random disks
+nohup nice -n 20 ./cmac_sim.sh random80_load &
